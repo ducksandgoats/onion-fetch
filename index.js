57e0386642d2698690dc003bbeac525896dfab09
ducksandgoats <mrtraderjoseph@gmail.com>
2022-10-07 23:01:35 -0500

    update the package

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const router = require('tor-request')
+// const router = require('tor-request')
 const makeFetch = require('make-fetch')
+const torAxios = require('tor-axios')
 // const fs = require('fs')
 // const path = require('path')
 
 module.exports = function makeGunFetch (opts = {}) {
   const DEFAULT_OPTS = { timeout: 30000 }
   const finalOpts = { ...DEFAULT_OPTS, ...opts }
-  const SUPPORTED_METHODS = ['HEAD', 'GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS']
+  // const SUPPORTED_METHODS = ['HEAD', 'GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS']
 //   const hostType = '_'
+  const tor = torAxios.torSetup({
+    ip: 'localhost',
+    port: 9050
+  })
   
   const useTimeOut = finalOpts.timeout
 
@@ -17,44 +22,49 @@ module.exports = function makeGunFetch (opts = {}) {
 
   const fetch = makeFetch(async (request) => {
 
-    const { url, method, headers, body } = request
+    // const { url, method, headers, body } = request
 
     try {
 
-      const { hostname, pathname, protocol, search, searchParams } = new URL(url)
+      const mainURL = new URL(request.url)
 
-      if ((protocol !== 'tor:' && protocol !== 'tors:') || !method || !SUPPORTED_METHODS.includes(method)) {
-        throw new Error(`request is not correct, protocol must be tor:// or tors://, method must be one of the following ${SUPPORTED_METHODS}`)
+      if ((mainURL.protocol !== 'tor:' && mainURL.protocol !== 'tors:') || !request.method) {
+        throw new Error(`request is not correct, protocol must be tor:// or tors://, or requires a method`)
       }
 
-      const mainProtocol = protocol.includes('s') ? 'https://' : 'http://'
+      const mainProtocol = protocol.includes('s') ? 'https:' : 'http:'
 
-      const opts = {url: mainProtocol + hostname + pathname, headers, qs: search, timeout: (headers['x-timer'] && headers['x-timer'] !== '0') || (searchParams.has('x-timer') && searchParams.get('x-timer') !== '0') ? Number(headers['x-timer'] || searchParams.get('x-timer')) * 1000 : useTimeOut}
+      request.url = request.url.replace(mainURL.protocol, mainProtocol)
 
-      if(method === 'POST' || method === 'PATCH' || method === 'PUT' || method === 'DELETE'){
-        opts.body = body
+      request.timeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (request.searchParams.has('x-timer') && request.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || request.searchParams.get('x-timer')) * 1000 : useTimeOut
+
+      if(request.method === 'POST' || request.method === 'PATCH' || request.method === 'PUT' || request.method === 'DELETE'){
+        const getTheBody = request.body
+        request.data = getTheBody
+        delete request.body
       }
 
-      const res = await new Promise((resolve, reject) => {
-        if(hostname.endsWith('.onion')){
-            router.torRequest(opts, (error, response) => {
-                if(error){
-                    reject(error)
-                } else {
-                    resolve(response)
-                }
-            })
-        } else {
-            router.request(opts, (error, response) => {
-                if(error){
-                    reject(error)
-                } else {
-                    resolve(response)
-                }
-            })
-        }
-    })
-    return {statusCode: res.statusCode, headers: res.headers, data: [res.body]}
+    //   const res = await new Promise((resolve, reject) => {
+    //     if(hostname.endsWith('.onion')){
+    //         router.torRequest(opts, (error, response) => {
+    //             if(error){
+    //                 reject(error)
+    //             } else {
+    //                 resolve(response)
+    //             }
+    //         })
+    //     } else {
+    //         router.request(opts, (error, response) => {
+    //             if(error){
+    //                 reject(error)
+    //             } else {
+    //                 resolve(response)
+    //             }
+    //         })
+    //     }
+    // })
+    const res = await tor.request(opts)
+    return {statusCode: res.statusCode, headers: res.headers, data: [res.data]}
     } catch (e) {
       return { statusCode: 500, headers: {}, data: [JSON.stringify(e)]}
     }
