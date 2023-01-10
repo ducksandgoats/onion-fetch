707eb58ce5ee3dcdfdaec2723dce8378da051bf5
ducksandgoats <mrtraderjoseph@gmail.com>
2023-01-09 20:41:58 -0600

    use got

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const makeFetch = require('make-fetch')
-const detect = require('detect-port')
-const axios = require('axios');
-const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent
-
-module.exports = function makeOnionFetch (opts = {}) {
-  const DEFAULT_OPTS = { timeout: 30000 }
-  const finalOpts = { ...DEFAULT_OPTS, ...opts }
+module.exports = async function makeOnionFetch (opts = {}) {
+  const makeFetch = require('make-fetch')
+  const {got} = await import('got')
+  const detect = require('detect-port')
+  const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent
+  const finalOpts = { timeout: 30000, ...opts }
   const mainData = {ip: 'localhost', port: 9050}
-  const tor = axios.create({ 'httpAgent': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'httpsAgent': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) })
   const useTimeOut = finalOpts.timeout
 
-  function takeCareOfIt(data){
-    console.log(data)
-    throw new Error('aborted')
-  }
-
-  function sendTheData(theSignal, theData){
-    if(theSignal){
-      theSignal.removeEventListener('abort', takeCareOfIt)
-    }
-    return theData
-  }
-
   const fetch = makeFetch(async (request) => {
-
-    if(request.signal){
-      request.signal.addEventListener('abort', takeCareOfIt)
-    }
     
     try {
 
@@ -39,25 +20,18 @@ module.exports = function makeOnionFetch (opts = {}) {
       if(mainURL.hostname === '_'){
         const detectedPort = await detect(mainData.port)
         const isItRunning = mainData.port !== detectedPort
-        return sendTheData(request.signal, {statusCode: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, data: [String(isItRunning)]})
+        return {statusCode: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, data: [String(isItRunning)]}
       }
 
       const mainProtocol = mainURL.protocol.includes('s') ? 'https:' : 'http:'
 
       request.url = request.url.replace(mainURL.protocol, mainProtocol)
 
-      request.timeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut
+      request.timeout = {request: (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut}
+      request.agent = { 'http': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'https': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) }
 
-      request.transformResponse = x => x
-
-      if(request.method === 'POST' || request.method === 'PATCH' || request.method === 'PUT' || request.method === 'DELETE'){
-        const getTheBody = request.body
-        request.data = getTheBody
-        delete request.body
-      }
-
-    const res = await tor.request(request)
-    return sendTheData(request.signal, {statusCode: res.status, headers: res.headers, data: [res.data]})
+      const res = await got(request)
+      return {statusCode: res.statusCode, headers: res.headers, data: [res.body]}
     } catch(e){
       const {mainHead, mainData} = (() => {
         if(request.headers.accept){
@@ -74,7 +48,7 @@ module.exports = function makeOnionFetch (opts = {}) {
           return {mainHead: 'text/plain; charset=utf-8', mainData: [e.name]}
         }
       })()
-      return sendTheData(request.signal, {statusCode: 500, headers: {'X-Error': e.name, 'Content-Type': mainHead}, data: mainData})
+      return {statusCode: 500, headers: {'X-Error': e.name, 'Content-Type': mainHead}, data: mainData}
     }
   })
 
