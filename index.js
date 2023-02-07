13827428c58fcb5ffe5165ab20871f2bb13ea85e
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-06 21:59:43 -0600

    update package

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = async function makeOnionFetch (opts = {}) {
   const { makeRoutedFetch } = await import('make-fetch')
   const {fetch, router} = makeRoutedFetch()
-  const {got} = await import('got')
+  const { default: nodeFetch } = await import('node-fetch')
   const detect = require('detect-port')
   const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent
   const finalOpts = { timeout: 30000, ...opts }
   const mainData = {ip: 'localhost', port: 9050}
   const useTimeOut = finalOpts.timeout
+  const mainAgents = { 'http': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'https': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) }
 
   function takeCareOfIt(data){
     console.log(data)
@@ -19,6 +20,16 @@ module.exports = async function makeOnionFetch (opts = {}) {
     }
     return theData
   }
+
+function useAgent(_parsedURL) {
+		if (_parsedURL.protocol === 'http:') {
+			return mainAgents.http;
+		} else if(_parsedURL.protocol === 'https:'){
+			return mainAgents.https;
+    } else {
+      throw new Error('protocol is not valid')
+    }
+	}
 
   async function handleTor(request) {
     const { url, method, headers: reqHeaders, body, signal, referrer } = request
@@ -33,23 +44,17 @@ module.exports = async function makeOnionFetch (opts = {}) {
         const detectedPort = await detect(mainData.port)
         const isItRunning = mainData.port !== detectedPort
         return {status: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, body: [String(isItRunning)]}
-      }
+    }
+    
+    request.url = request.url.replace('tor', 'http')
+    request.agent = useAgent
+    const mainTimeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut
 
-      request.url = request.url.replace('tor', 'http')
-
-      request.timeout = {request: (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut}
-      request.agent = { 'http': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'https': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) }
-
-      delete request.referrer
-      if(request.method === 'CONNECT' || request.method === 'GET' || request.method === 'HEAD' || request.method === 'OPTIONS' || request.method === 'TRACE'){
-        delete request.body
-      }
-      if(!request.signal){
-        delete request.signal
-      }
-
-      const res = await got(request)
-      return sendTheData(signal, {status: res.statusCode, headers: res.headers, body: [res.body]})
+    const res = await Promise.race([
+      nodeFetch(request.url, request),
+      new Promise((resolve) => setTimeout(resolve, mainTimeout))
+    ])
+      return sendTheData(signal, {status: res.status, headers: res.headers, body: [res.body]})
   }
 
   async function handleTors(request) {
@@ -68,20 +73,14 @@ module.exports = async function makeOnionFetch (opts = {}) {
       }
 
       request.url = request.url.replace('tor', 'http')
+    request.agent = useAgent
+    const mainTimeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut
 
-      request.timeout = {request: (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut}
-      request.agent = { 'http': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'https': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) }
-
-      delete request.referrer
-      if(request.method === 'CONNECT' || request.method === 'GET' || request.method === 'HEAD' || request.method === 'OPTIONS' || request.method === 'TRACE'){
-        delete request.body
-      }
-      if(!request.signal){
-        delete request.signal
-      }
-
-    const res = await got(request)
-    return sendTheData(signal, {status: res.statusCode, headers: res.headers, body: [res.body]})
+    const res = await Promise.race([
+      nodeFetch(request.url, request),
+      new Promise((resolve) => setTimeout(resolve, mainTimeout))
+    ])
+    return sendTheData(signal, {status: res.status, headers: res.headers, body: [res.body]})
   }
   
   router.any('tor://*/**', handleTor)
