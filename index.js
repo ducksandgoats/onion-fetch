cc0bba79963d349097d05e0941a87eb1d6de0f09
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-13 17:05:44 -0600

    update the package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = async function makeOnionFetch (opts = {}) {
   const { makeRoutedFetch } = await import('make-fetch')
-  const {fetch, router} = makeRoutedFetch()
+  const {fetch, router} = makeRoutedFetch({onNotFound: handleEmpty, onError: handleError})
   const { default: nodeFetch } = await import('node-fetch')
   const detect = require('detect-port')
   const SocksProxyAgent = require('socks-proxy-agent').SocksProxyAgent
@@ -8,6 +8,37 @@ module.exports = async function makeOnionFetch (opts = {}) {
   const mainData = {ip: '127.0.0.1', port: 9050}
   const useTimeOut = finalOpts.timeout
   const mainAgents = { 'http': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`), 'https': new SocksProxyAgent(`socks5h://${mainData.ip}:${mainData.port}`) }
+
+  function handleEmpty(request) {
+    const { url, headers: reqHeaders, method, body, signal } = request
+    if(signal){
+      signal.removeEventListener('abort', takeCareOfIt)
+    }
+    const mainReq = !reqHeaders.has('accept') || !reqHeaders.get('accept').includes('application/json')
+    const mainRes = mainReq ? 'text/html; charset=utf-8' : 'application/json; charset=utf-8'
+    return {status: 400, headers: { 'Content-Type': mainRes }, body: mainReq ? `<html><head><title>${url}</title></head><body><div><p>did not find any data</p></div></body></html>` : JSON.stringify('did not find any data')}
+  }
+
+  function handleError(e, request) {
+    const { url, headers: reqHeaders, method, body, signal } = request
+    if(signal){
+      signal.removeEventListener('abort', takeCareOfIt)
+    }
+    const mainReq = !reqHeaders.has('accept') || !reqHeaders.get('accept').includes('application/json')
+    const mainRes = mainReq ? 'text/html; charset=utf-8' : 'application/json; charset=utf-8'
+    return {status: 500, headers: { 'X-Error': e.name, 'Content-Type': mainRes }, body: mainReq ? `<html><head><title>${e.name}</title></head><body><div><p>${e.stack}</p></div></body></html>` : JSON.stringify(e.stack)}
+  }
+
+  async function handleData(timeout, data) {
+    if (timeout) {
+      return await Promise.race([
+        new Promise((resolve, reject) => setTimeout(() => { const err = new Error('timed out'); err.name = 'timeout'; reject(err) }, timeout)),
+        data
+      ])
+    } else {
+      return await data
+    }
+  }
 
   function takeCareOfIt(data){
     console.log(data)
@@ -49,9 +80,9 @@ function useAgent(_parsedURL) {
     request.agent = useAgent
     const useLink = request.url.replace('tor', 'http')
     delete request.url
-    const mainTimeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut
+    const mainTimeout = reqHeaders.has('x-timer') || mainURL.searchParams.has('x-timer') ? reqHeaders.get('x-timer') !== '0' || mainURL.searchParams.get('x-timer') !== '0' ? Number(reqHeaders.get('x-timer') || mainURL.searchParams.get('x-timer')) * 1000 : undefined : useTimeOut
     
-    return sendTheData(signal, await Promise.race([nodeFetch(useLink, request), new Promise((resolve, reject) => setTimeout(() => { reject(new Error('timeout')) }, mainTimeout))]))
+    return sendTheData(signal, await handleData(mainTimeout, nodeFetch(useLink, request)))
   }
 
   async function handleTors(request) {
